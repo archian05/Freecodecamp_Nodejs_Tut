d39b799b8ee1a99baafe072b45c9a225ad46cbc0
archian05 <balapandian.g@gmail.com>
2022-07-10 07:34:55 +0530

    nodemon added

--- 1-intro.js
+++ 1-intro.js
@@ -1,9 +1,8 @@
 const amount =12
-
 if (amount < 10 ){
 console.log('small number')
 }
 else{
     console.log('large number')
 }
-console.log(`Its my first program`)
+console.log(`Its my first program with nodemon`)
